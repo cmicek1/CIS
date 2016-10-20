07b19d79c54a05c60971f4b1a477cade61e4628c
Chris Micek <cmicek325@verizon.net>
2016-10-20 19:29:54 -0400

    Got it backwards

--- PA1/registration.py
+++ PA1/registration.py
@@ -12,7 +12,7 @@ def register(a, b):
 
     # Method using SVD to directly solve for R
 
-    sigma = np.sum(np.apply_along_axis(scialg.norm, 0, a_tilde)) / np.sum(np.apply_along_axis(scialg.norm, 0, b_tilde))
+    sigma = np.sum(np.apply_along_axis(scialg.norm, 0, b_tilde)) / np.sum(np.apply_along_axis(scialg.norm, 0, a_tilde))
 
     H = (sigma * a).dot(b.T)
 
