818452f0c1306277c5a263530d55dd544f3a0edc
juliekshade <jshade1@jhu.edu>
2016-10-21 13:59:20 -0400

    started pivot calibration methods (pivot_cal.py)

--- PA1/pivot_cal.py
+++ PA1/pivot_cal.py
@@ -1,5 +1,22 @@
 import numpy as np
 import scipy.linalg as scialg
+import registration as reg
 
-def pivot(a):
-    #a is a set of pivot calibration points
+def pivot(g):
+    #g is a set of pivot calibration points
+    G_0 = np.mean(g, axis=1, keepdims=True)
+
+    print G_0
+
+    G_j = g - G_0
+
+    print G_j
+
+    R, p = reg.register(g, G_j)
+
+    print R
+    print p
+
+
+
+
