8878e246b81416938f1aee34f3fa69800a459e09
juliekshade <jshade1@jhu.edu>
2016-10-21 13:11:44 -0400

    added methods for transformations with given R and p

--- PA1/frame_transformations.py
+++ PA1/frame_transformations.py
@@ -1,10 +1,15 @@
 import numpy as np
-
+import scipy.linalg as scialg
 
 def skew(v):
     return np.array([[0, -v[2], v[1]],
                      [v[2], 0, -v[0]],
                      [-v[1], v[0], 0]])
 
-def rotate(R, p, v):
+def transform(R, p, v):
+    return R.dot(v) + p
+
+def inv_transform(R, p, v):
+    R = scialg.inv(R)
+    p = scialg.inv(R).dot(p)
     return R.dot(v) + p
