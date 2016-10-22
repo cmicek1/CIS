858ef5550204762252156b3334d89a3b1433e49b
juliekshade <jshade1@jhu.edu>
2016-10-22 14:16:28 -0400

    almost finished file output

--- PA1/PA1_driver.py
+++ PA1/PA1_driver.py
@@ -12,13 +12,13 @@ def tofile(outfile, calbody, calreadings, empivot, optpivot):
     f = open(outfile, 'w')
     h, t = os.path.split(outfile)
     f.write('{0}, {1}, {2}\n'.format(len(c_exp[0].data[0]), len(c_exp), t))
-    f.write('{0},   {1},   {2}\n'.format(format(p_em[0], '.2f'), format(p_em[1], '.2f'), format(p_em[2], '.2f')))
+    f.write('  {0},   {1},   {2}\n'.format(format(p_em[0], '.2f'), format(p_em[1], '.2f'), format(p_em[2], '.2f')))
     f.write('optical output goes here \n')
     #f.write('{0},   {1},   {2}'.format(p_opt[0], p_opt[1], p_opt[2]))
     #insert loop for writing c_exp in correct format
 
     for i in range(len(c_exp)):
         for k in range(len(c_exp[0].data[1])):
-            f.write('{0},   {1},   {2}\n'.format(format(c_exp[i].data[0][k], '.2f'), format(c_exp[i].data[1][k], '.2f'),
+            f.write('  {0},   {1},   {2}\n'.format(format(c_exp[i].data[0][k], '.2f'), format(c_exp[i].data[1][k], '.2f'),
                                                                                             format(c_exp[i].data[2][k], '.2f')))
     f.close()
