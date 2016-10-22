2f3e3c832da6a1f98d3ef22bb4e3860b72de56c2
juliekshade <jshade1@jhu.edu>
2016-10-21 22:48:34 -0400

    finished PA_prob5 and fixed some parts of pivot_cal

--- PA1/PA1_Prob5.py
+++ PA1/PA1_Prob5.py
@@ -5,15 +5,13 @@ def p_dimple(empivot_file):
     """
     Computes position of dimpled pin in tracker coordinate system.
 
-    :param calbody_file: File name/path for the calibration object data file
-    :param calreadings_file: File name/path for the readings from the trackers
+    :param empivot_file filename/path for file with positions of probe in EM tracker coordinates
 
     :type calbody_file: str
-    :type calreadings_file: str
 
-    :return: C_expected, a list of the expected positions of EM tracking markers on the calibration object for each
-             frame of data
-    :rtype: [PointCloud.PointCloud]
+    :return: p_piv, the position of the dimpled pin in tracker coordinates
+
+    :rtype: np.array
     """
     g_clouds = pc.fromfile(empivot_file)
 
