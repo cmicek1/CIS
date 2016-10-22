6e97359f4338ff89ade5aa70051166542503e69a
juliekshade <jshade1@jhu.edu>
2016-10-22 12:30:39 -0400

    started driver/file output

--- PA1/PA1_driver.py
+++ PA1/PA1_driver.py
@@ -1,5 +1,12 @@
 import pandas as pd
+import PA1_Prob4 as p4
+import PA1_Prob5 as p5
+import PA1_Prob6 as p6
+
+def tofile(outfile, calbody, calreadings, empivot, optpivot):
+
+    c_exp = p4.c_expected(calbody, calreadings)
+    p_em = p5.p_dimple(empivot)
+    p_opt = p6.p_dimple(optpivot, calbody, calreadings)
 
 
-def tofile(outfile, calbody, calreadings, empivot, ):
-    c_exp =
