a2a6b76ceeb3e3a621250037f8b7b3c89596c5d8
Chris Micek <cmicek325@verizon.net>
2016-10-26 02:17:22 -0400

    Slight modifications; P5 is success

--- PA2/PA2_Prob5.py
+++ PA2/PA2_Prob5.py
@@ -3,12 +3,12 @@ import PointCloud as pc
 import distortion as d
 
 
-def tip_in_EM(emfiducialss, ptip, coeffs, q_min, q_max, q_star_min, q_star_max):
+def find_freg(ctfiducials, Cs, coeffs, q_min, q_max, q_star_min, q_star_max):
     """
-    Returns the position of the pointer tip in EM coordinates for tracker data frames when the tip is on a fiducial pin.
+    Returns the transformation from tracker coordinates to CT coordinates.
 
-    :param emfiducialss: The file name/path of the file with marker positions when the pointer is on the fiducials,
-                         relative to the EM tracker.
+    :param ctfiducials: The file name/path of the file with marker positions when the pointer is on the fiducials,
+                        relative to the CT frame.
     :param ptip: The coordinates of the tip of the pointer relative to the pointer coordinate system (Output of
                  pivot_cal.pivot)
     :param coeffs: Coefficient matrix for dewarping (Output of distortion.distcal)
@@ -17,7 +17,7 @@ def tip_in_EM(emfiducialss, ptip, coeffs, q_min, q_max, q_star_min, q_star_max):
     :param q_star_min: Vector of output minima for the initial correction matrix creation (Output of distortion.distcal)
     :param q_star_max: Vector of output maxima for the initial correction matrix creation (Output of distortion.distcal)
 
-    :type emfiducialss: str
+    :type ctfiducials: str
     :type ptip: numpy.array(numpy.float64) shape (3,) or (, 3)
     :type coeffs: numpy.array([numpy.float64][]) degree**3 x 3
     :type q_min: numpy.array(numpy.float64) shape (3,) or (, 3)
@@ -29,19 +29,7 @@ def tip_in_EM(emfiducialss, ptip, coeffs, q_min, q_max, q_star_min, q_star_max):
              frame
     """
 
-    G = d.correct(emfiducialss, coeffs, q_min, q_max, q_star_min, q_star_max)
-    G_0 = np.mean(G[0][0].data, axis=1, keepdims=True)
+    b = d.correct(ctfiducials, coeffs, q_min, q_max, q_star_min, q_star_max)
+    f_reg = Cs.register(b[0][0])
 
-    G_j = G[0][0].data - G_0
-
-    Cs = pc.PointCloud()
-
-    for frame in G:
-        F = frame[0].register(pc.PointCloud(G_j))
-        C = pc.PointCloud(ptip.reshape((3, 1))).transform(F)
-        if Cs.data is None:
-            Cs.data = C.data
-        else:
-            Cs = Cs.add(C)
-
-    return Cs
+    return f_reg
