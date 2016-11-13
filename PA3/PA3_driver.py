453bbcc61ab4df55b0b5c3e05eb4793ef0756809
juliekshade <jshade1@jhu.edu>
2016-11-13 14:11:20 -0500

    Wrote file reading code for surface mesh

--- PA3/PA3_driver.py
+++ PA3/PA3_driver.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def readInput(fpath):
     """
@@ -9,11 +10,28 @@ def readInput(fpath):
     :return: vCoords: An array that holds the coordinates of each vertex
     :return: vIndices: An array that holds the indices of the coordinates of the three vertices for each triangle
 
-    :rtype: np.array(float64)
+    :rtype: np.array(float64) 3 x nNvertices
+    :rtype: np.array(int) 3 x nTriangles
     """
 
+    f = open(fpath, 'r')
 
+    nVertices = int(f.readline())
 
+    vCoords = np.zeros([3, nVertices])
 
+    for i in range(nVertices):
+        vertex = f.readline().split()
+        for j in range(0, 3):
+            vCoords[j][i] = np.float64(vertex[j])
 
-    return vCoords,
+    nTriangles = int(f.readline())
+
+    vIndices = np.zeros([3, nTriangles])
+
+    for i in range(nTriangles):
+        triangle = f.readline().split()
+        for j in range(0, 3):
+            vIndices[j][i] = int(triangle[j])
+
+    return vCoords, vIndices
