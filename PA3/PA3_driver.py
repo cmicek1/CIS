c48afbaa107d156ceb458bcf1415a08b15dfca30
juliekshade <jshade1@jhu.edu>
2016-11-13 13:43:30 -0500

    Beginning framework for PA3

---
+++ PA3/PA3_driver.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def readInput(fpath):
+    """
+    Extract two arrays with information about triangles from an input file.
+    :param fpath: The file path to the input data.
+    :type fpath: str
+
+    :return: vCoords: An array that holds the coordinates of each vertex
+    :return: vIndices: An array that holds the indices of the coordinates of the three vertices for each triangle
+
+    :rtype: np.array(float64)
+    """
+
+
+
+
+
+    return vCoords,
