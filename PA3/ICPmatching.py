fixed bug with calculation of tip in B coordinate system

PA3/ICPmatching.py
@@ -3,13 +3,15 @@ import numpy.linalg as numalg
 import PointCloud as pc
 import Frame as fr
 
-def findTipB(aFrames, ledA, tipA):
+def findTipB(aFrames, bFrames, ledA, tipA, ledB, tipB):
 
     d_ks = np.zeros([3, len(aFrames)])
 
     for i in range(len(aFrames)):
         regA = ledA.register(aFrames[i])
-        d_k = regA.r.dot(tipA.data) + regA.p
+        regB = ledB.register(bFrames[i])
+        reg = regB.inv.compose(regA)
+        d_k = reg.r.dot(tipA.data) + reg.p
         for j in range(0, 3):
             d_ks[j][i] = d_k[j]
 
@@ -24,7 +26,7 @@ def computeSamplePoints(d_k, freg):
 
 def findClosestPoint(s_i, vCoords, vInd):
     """
-    Finds the closest point to p on a mesh defined by vCoords (coordinates of vertices) and vInd (vertex indices for
+    Finds the closest point to s_i on a mesh defined by vCoords (coordinates of vertices) and vInd (vertex indices for
     each triangle.
     :param p:
     :param vCoords:
@@ -70,9 +72,8 @@ def findClosestPoint(s_i, vCoords, vInd):
 
     return minPoint
 
-
-
 def projectOnSegment(c, p, q):
+
     l = ((c-p).dot(q-p))/((q-p).dot(q-p))
     lambda_star = max(0, min(l, 1))
     c_star = p + lambda_star*(q-p)
@@ -80,10 +81,17 @@ def projectOnSegment(c, p, q):
 
 
 def ICPmatch(s_i, vCoords, vInd):
+
     c_ij = np.zeros([3, np.shape(s_i)[1]])
     for i in range(np.shape(s_i)[1]):
-       # c = np.zeros([3, 1])
         c = findClosestPoint(s_i[:,i], vCoords, vInd)
         c_ij[:, i] = c[:]
 
     return c_ij
+
+def calcDifference(c_kPoints, d_kPoints):
+    dist = np.zeros(np.shape(c_kPoints)[1])
+    for i in range(np.shape(c_kPoints)[1]):
+        dist[i] = numalg.norm(d_kPoints[:, i] - c_kPoints[:, i])
+
+    return dist

PA3/PA3_driver.py
@@ -5,7 +5,12 @@ import numpy as np
 import numpy.linalg as numalg
 import sys, os
 
-def main(meshfile, bodyA, bodyB, sampleData, outfile):
+def main():
+    """
+    Main method, takes command line arguments to either run tests or run program with a given data set (x-ddddd).
+    :return:
+    """
+def manual(meshfile, bodyA, bodyB, sampleData, outfile):
     """
     Super simple driver, @chris please make this better, I'm just using it for testing for now.
     :param meshfile:
@@ -21,7 +26,7 @@ def main(meshfile, bodyA, bodyB, sampleData, outfile):
 
     aFrames, bFrames = icpf.readSample(sampleData, nledA, nledB)
 
-    d_kPoints = icp.findTipB(aFrames, ledA, tipA)
+    d_kPoints = icp.findTipB(aFrames, bFrames, ledA, tipA, ledB, tipB)
 
     I = fr.Frame(np.identity(3), np.zeros(3))
 
@@ -29,9 +34,7 @@ def main(meshfile, bodyA, bodyB, sampleData, outfile):
 
     c_kPoints = icp.ICPmatch(s_i, vCoords, vIndices)
 
-    dist = np.zeros(np.shape(c_kPoints)[1])
-    for i in range(np.shape(c_kPoints)[1]):
-        dist[i] = numalg.norm(d_kPoints[:,i] - c_kPoints[:,i])
+    dist = icp.calcDifference(c_kPoints, d_kPoints)
 
     writefile(d_kPoints, c_kPoints, dist, outfile)
 
@@ -42,10 +45,8 @@ def writefile(d_k, c_k, dist, outfile):
     f.write('{0} {1}\n'.format(np.shape(d_k)[1], t))
 
     for i in range(np.shape(d_k)[1]):
-        f.write('{0:>8},{1:>9},{2:>9}'.format(format(d_k[0][i], '.2f'), format(d_k[1][i], '.2f'), format(d_k[2][i], '.2f')))
-        f.write('{0:>13},{1:>9},{2:>9}'.format(format(c_k[0][i], '.2f'), format(c_k[1][i], '.2f'), format(c_k[2][i], '.2f')))
+        f.write('{0:>8}{1:>9}{2:>9}'.format(format(d_k[0][i], '.2f'), format(d_k[1][i], '.2f'), format(d_k[2][i], '.2f')))
+        f.write('{0:>13}{1:>9}{2:>9}'.format(format(c_k[0][i], '.2f'), format(c_k[1][i], '.2f'), format(c_k[2][i], '.2f')))
         f.write('{0:>10}\n'.format(format(dist[i], '.3f')))
-
-
 
     f.close()