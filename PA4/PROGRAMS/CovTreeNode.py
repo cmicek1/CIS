a0f63f485c9a018203440eca25ba37dea9226206
Chris Micek <cmicek325@verizon.net>
2016-11-27 16:08:56 -0500

    Backbone for covariance tree added

---
+++ PA4/PROGRAMS/CovTreeNode.py
@@ -0,0 +1,64 @@
+import numpy as np
+import Frame as fr
+import PointCloud as pc
+
+
+class CovTreeNode:
+    def __init__(self, triangles, num_tri):
+        self.triangle_list = triangles
+        self.num_tri = num_tri
+        self.frame = self._FindCovFrame()
+        self.has_subtrees = False
+        self.subtrees = [None, None]
+
+    def _FindCovFrame(self, *args):
+        if len(args) == 2:
+            points = []
+            num_points = args[1]
+            for i in range(num_points):
+                points.append(self.triangle_list[i].data.tolist())
+
+            points = np.array(points).squeeze().T
+
+            return self._FindCovFrame(points, num_points)
+
+        elif len(args) == 3:
+            points = args[1][:, 0:args[2] + 1]
+            c = np.mean(points, axis=1, keepdims=True)
+            u = points - c
+
+            A = u.dot(u.T)
+
+            evals, evecs = np.linalg.eig(A)
+
+            inds = evals.argsort()[::-1]
+
+            max_evec = evecs[inds[0]]
+
+            coeffs = np.zeros((3, 9))
+            coeffs[[0, 0], [1, 3], [2, 6]] = 1.0
+
+            r = np.linalg.lstsq(coeffs, max_evec)
+
+            return fr.Frame(r, c)
+
+    def _FindBoundingBox(self, n):
+        LB = pc.PointCloud(self.triangle_list[0].SortPoint()).transform(self.frame.inv)
+        bounds = [LB, LB]
+        for k in range(n):
+            bounds = self.triangle_list[k].EnlargeBounds(self.frame, bounds)
+
+        return bounds
+
+    def _SplitSort(self, num):
+        points = []
+        for k in range(num):
+            points.append(pc.PointCloud(self.triangle_list[k].SortPoint()).transform(self.frame.inv).data.tolist())
+        points = np.array(points).squeeze().T
+        inds = np.argsort(points[0, :])
+
+
+    def _ConstructSubtrees(self):
+        if len(self.triangle_list) <= 1:
+            return
+
