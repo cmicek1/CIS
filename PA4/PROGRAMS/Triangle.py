474a9612e3f7b645c6af36f12d1c600c01a675b0
juliekshade <jshade1@jhu.edu>
2016-11-30 13:09:07 -0500

    added documentation to most files

--- PA4/PROGRAMS/Triangle.py
+++ PA4/PROGRAMS/Triangle.py
@@ -31,11 +31,33 @@ class Triangle:
 
 
     def ClosestPointTo(self, v):
+        """
+        Finds the closest point to a vector v on a Triangle.
+
+        :param v: vector to find the closest point to.
+        :type v: np.array(np.float64) 3 X 1
+
+        :return: The closest point on this Triangle to the point v
+        :rtype: np.array(np.float64) 3 X 1
+        """
         data = self.corners.data
         p, q, r = data[:, 0], data[:, 1], data[:, 2]
         return icpm.minPointonTriangle(v, p, q, r)
 
+
     def EnlargeBounds(self, frame, bounds):
+        """
+        Increases the size of a given bounding box based on the bounds of this Triangle.
+
+        :param frame: Frame of the bounding box
+        :param bounds: upper and lower bounds to expand if needed
+
+        :type frame: fr.Frame
+        :type bounds: np.array(np.float64) 2 X 1
+
+        :return: the newly expanded (if necessary) bounds
+        :rtype: np.array(np.float64) 2 X 1
+        """
         FiC = self.corners.transform(frame.inv)
         for i in range(3):
             bounds[0] = np.amin(np.hstack((bounds[0], FiC.data[:, i].reshape((3, 1)))), axis=1, keepdims=True)
@@ -43,11 +65,34 @@ class Triangle:
 
         return bounds
 
+
     def BoundingBox(self, frame):
+        """
+        Defines a bounding box around this triangle.
+
+        :param frame: the frame that the bounding box should be in reference to.
+        :type frame: fr.Frame
+
+        :return: the new upper and lower bounds of the bounding box
+        :rtype: np.array(np.float64) 2 X 1
+        """
         return self.EnlargeBounds(frame, [np.array([[np.inf], [np.inf], [np.inf]]),
                                           np.array([[-np.inf], [-np.inf], [-np.inf]])])
 
+
     def MayBeInBounds(self, frame, bounds):
+        """
+        Determines if this Triangle may be in a given bounding box.
+
+        :param frame: The reference frame of the bounding box.
+        :param bounds: The upper and lower bounds of the bounding box.
+
+        :type frame: fr.Frame
+        :type bounds: np.array(np.float64) 2 X 1
+
+        :return: True if may be in bounds, False otherwise
+        :rtype: bool
+        """
         FiC = self.corners.transform(frame.inv)
         for i in range(3):
             if np.all(FiC.data[:, i] >= bounds[0]) and np.all(FiC.data[:, i] <= bounds[1]):
